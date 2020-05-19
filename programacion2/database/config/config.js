7ad5c5943d31bdec2a274d28461df7072004d65e
facundo maleh <facumaleh@gmail.com>
2020-05-19 19:30:20 -0300

    id de las reviews

--- programacion2/database/config/config.js
+++ programacion2/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "root",
     "database": "integrador",
     "host": "localhost",
     "port": 3309,
